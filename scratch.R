b9f0436be8f4c27be72c74a3671ff7317d89d762
beckyfisher <rebeccafisher76@gmail.com>
2019-09-04 08:34:02 +0800

    scratch file for setting arguments for trouble shooting functions

--- scratch.R
+++ scratch.R
@@ -6,6 +6,15 @@ y.var="suc"
 trials.var="tot"
 x.type = "gamma"
 y.type = "binomial"
+params = c("top", "beta", "NEC")
 burnin = 1000
 n.iter = 2000
+
+
+
+CI=TRUE
+posterior.median=TRUE
+median.model=FALSE
+add.NEC=TRUE
+
 
