8b842c2099f16d17220bb6e96905a0773108d971
beckyfisher <rebeccafisher76@gmail.com>
2019-09-13 19:33:41 +0800

    Added the ability to change the default probability value to test for a difference between between and within chains.

--- R/check_mixing.R
+++ R/check_mixing.R
@@ -1,14 +1,16 @@
 #' check.mixing
 #'
-#' Generates a plot of MCMC chains and ACF function for a Jags model output.
+#' Calculates the within and between chain mixing and tests of the between is greater than within based on the supplied quantile probability value.
 #'
 #' @param  J a jag model fit as returned by a call to jags fit.jagsNEC
 #'
+#' @param  prob.val the probability value to pass to quantile to assess the between chain coefficient of variation against the within chain coefficient of variation. Please inspect the model code for more details.
+#'
 #' @export
-#' @return A plot of MCMC chains and ACF diagrames for each element in params.
+#' @return A list of within and between coefficient of variations for parameter, the outcome of the test if between is greater than within, and the ratio ratio.
 
 
-check.mixing <- function(J){
+check.mixing <- function(J, prob.val=0.75){
   x <- J$BUGSoutput$sims.array
   params <- J$parameters.to.save
   num.chains <- ncol(x[,,params[1]])
@@ -29,7 +31,7 @@ check.mixing <- function(J){
   names(between.cv) <- params
   
   cv.test <- lapply(params, FUN=function(i){
-    between.cv[[i]]>quantile(within.cv[[i]], probs = 0.75)
+    between.cv[[i]]>quantile(within.cv[[i]], probs = prob.val)
   })
   names(cv.test) <- params
     
