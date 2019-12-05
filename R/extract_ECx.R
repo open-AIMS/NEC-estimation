a7de75dd38aa96af1bdcfc9308f419d89d08746f
beckyfisher <rebeccafisher76@gmail.com>
2019-12-05 16:49:14 +0800

    Changed definition and calculation of absolute ECx values

--- R/extract_ECx.R
+++ R/extract_ECx.R
@@ -7,7 +7,7 @@
 #' 
 #' @param ECx.val the desired percentage effect value. This must be a value between 1 and 99, defaults to 10.
 #' 
-#' @param type a character vector indicating if relative or absolute values for the ECx should be calculated. Takes either "relative" or "absolute"
+#' @param type a character vector indicating if relative or absolute values for the ECx should be calculated. Takes values of either "relative" or "absolute", with "absolute" being the default. For the default model type fit by jagsNEC, "relative" is calculated as the percentage decrease from the maximum value of the response *top* to the minimum predicted value of the response. For the default model fit, "absolute" is calculated as the the percentage decrease from the maximum value of the response *top* to *0*. If a 4 parameter model is fit, absolute will be calculated as the percent drecrease from *top* to *bot*, and should return the same results as type "relative"
 #' 
 #' @param precision The number of unique x values over which to find ECx - large values will make the ECx estimate more precise.
 #' 
@@ -18,13 +18,12 @@
 #' 
 #' @details Please note that the estimated ECx value is based on the equivalent percentage decrease from the range of the highest to the lowest estimate value across the range of the observed concentration (x) values. If the concentration response relationship is such that the full range of observed responses is not captured (ie a complete decline response at the highest level of exposure), the estimated ECx values may be lower than if the full concentration-response curve were available. Note this is therefore a conservative value.
 
-extract_ECx <- function(X, ECx.val=10, precision=10000, posterior = FALSE, type="relative"){
+extract_ECx <- function(X, ECx.val=10, precision=10000, posterior = FALSE, type="absolute"){
   if(ECx.val<1 | ECx.val>99){
     stop("Supplied ECx.val is not in the required range. Please supply a percentage value between 1 and 99.")
   } 
-  if(is.na(match(X$y.type, c("binomial", "beta"))) & type=="absolute"){
-    stop("Absolute ECx values are only valid for response variables that are contained from 0 to 1, 
-         such as a binomial or beta") 
+  if(X$y.type=="gaussian" & X$model != "4param" & type=="absolute"){
+    stop("Absolute ECx values are not valid for a gaussian response variable unless a 4 parameter model is fit") 
   }
 
   label <- paste("EC",ECx.val,sep="")
@@ -39,19 +38,27 @@ extract_ECx <- function(X, ECx.val=10, precision=10000, posterior = FALSE, type=
      ECx.y <- max(range.y)-diff(range.y)*(ECx.val/100)
      ECx.x <- x.vec[which.min(abs(y-ECx.y))]
      return(ECx.x)
-  
-   })    
+     })    
   }
 
-  if(type=="absolute"){
-    ECx.out <- apply(pred.vals$posterior, MARGIN=2, FUN=function(y){
-      range.y <- range(y)
-      ECx.y <- 1-(ECx.val/100)
-      ECx.x <- x.vec[which.min(abs(y-ECx.y))]
-      return(ECx.x)
-      
-    })    
+  if(type=="absolute" & X$model == "4param"){
+     ECx.out <- apply(pred.vals$posterior, MARGIN=2, FUN=function(y){
+     range.y <- range(y)
+     ECx.y <- max(range.y)-diff(range.y)*(ECx.val/100)
+     ECx.x <- x.vec[which.min(abs(y-ECx.y))]
+     return(ECx.x)  
+     })     
   }
+  
+  if(type=="absolute" & X$model != "4param"){
+    ECx.out <- apply(pred.vals$posterior, MARGIN=2, FUN=function(y){
+      range.y <- c(0, max(y))
+      ECx.y <- max(range.y)-diff(range.y)*(ECx.val/100)
+      ECx.x <- x.vec[which.min(abs(y-ECx.y))]
+      return(ECx.x)  
+    })     
+  }  
+
   
   ECx.estimate <- quantile(ECx.out, probs=c(0.5, 0.025, 0.975))
   names(ECx.estimate) <- c(label, paste(label, "lw", sep="_"), paste(label, "up", sep="_"))
