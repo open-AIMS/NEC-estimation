818dd902f68519ac67ce297ff8e5ce1a0d569b61
beckyfisher <rebeccafisher76@gmail.com>
2019-12-13 12:04:23 +0800

    Edits to accomodate the ECx model

--- R/plot_jagsNECfit.R
+++ R/plot_jagsNECfit.R
@@ -26,23 +26,36 @@
 #' @return a plot of the fitted model
 
 plot.jagsNECfit <- function(X,  CI=TRUE,  posterior.median=TRUE,  median.model=FALSE,  
-                         add.NEC=TRUE, NEC.loc="topright",
+                         add.NEC=TRUE, legend.loc="topright",  add.EC10=FALSE,
                          xform=NA, lxform=NA,
                          jitter.x=FALSE, jitter.y=FALSE, 
                          ylab="response", 
                          xlab="concentration",  ...){
-  
+
+  if(X$model=="basic4param" & add.NEC==TRUE){
+      add.NEC=FALSE; add.EC10=TRUE
+  }
+
   # check if y.type is binomial
   y.type <- X$y.type
   if(y.type=="binomial"){
     y.dat <- X$mod.dat$y/X$mod.dat$trials}else{
     y.dat <- X$mod.dat$y}
   
+  EC10 <- c(NA, NA, NA)
+  if(add.EC10==TRUE & X$y.type!="gaussian"){
+   EC10 <- extract_ECx(X)
+  }
+  if(add.EC10==TRUE & X$y.type=="gaussian"){
+    EC10 <- extract_ECx(X, type="relative")
+  }  
+  
   # check if a transformation is required for x
   if(class(xform)=="function"){
     x.dat <- xform(X$mod.dat$x)
     NEC <- xform(X$NEC)
     x.vec <- xform(X$pred.vals$x)
+    EC10 <- xform(EC10)
   }else{
     x.dat <- X$mod.dat$x
     NEC <- X$NEC
@@ -58,10 +71,13 @@ plot.jagsNECfit <- function(X,  CI=TRUE,  posterior.median=TRUE,  median.model=F
        pch=16, xaxt="n",
        col=adjustcolor(1, alpha=0.25), 
        cex=1.5) 
+  
   if(class(lxform)!="function"){
     axis(side=1)
     NEC.legend <- paste("NEC: ", signif(NEC[2],3), 
                         " (", signif(NEC[1],3),"-", signif(NEC[3],3),")",sep="")
+    EC10.legend <- paste("EC10: ", signif(EC10[2],3), 
+                         " (", signif(EC10[1],3),"-", signif(EC10[3],3),")",sep="")
     }else{
     x.ticks <- seq(min(x.dat), max(x.dat), length=7)
     x.labs <- signif(lxform(x.ticks),2)
@@ -69,9 +85,10 @@ plot.jagsNECfit <- function(X,  CI=TRUE,  posterior.median=TRUE,  median.model=F
     NEC.legend <- paste("NEC: ", signif(lxform(NEC[2]),3), 
                         " (",    signif(lxform(NEC[1]),3),"-", 
                                  signif(lxform(NEC[3]),3),")",sep="")    
+    EC10.legend <- paste("EC10: ", signif(lxform(EC10[2]),3), 
+                        " (",    signif(lxform(EC10[1]),3),"-", 
+                        signif(lxform(EC10[3]),3),")",sep="")  
   }
-  
-  abline(v=NEC, col = "red", lty=c(3,1,3))   
   
   if(CI==TRUE){
     lines(x.vec, X$pred.vals$up, lty=2) 
@@ -83,9 +100,22 @@ plot.jagsNECfit <- function(X,  CI=TRUE,  posterior.median=TRUE,  median.model=F
   if(median.model==TRUE){
     lines(x.vec, X$pred.vals$y.m, col="red")  
   }
-  if(add.NEC==TRUE){
-        legend(NEC.loc, bty="n",
-           legend=NEC.legend)
+  if(add.NEC==TRUE & add.EC10==FALSE){
+    abline(v=NEC, col = "red", lty=c(3,1,3))   
+    legend(legend.loc, bty="n",
+           legend=NEC.legend, lty=1, col="red")
   }
-
+  if(add.EC10==TRUE & add.NEC==FALSE){
+    abline(v=EC10, col = "red", lty=c(3,1,3))  
+    legend(legend.loc, bty="n",
+          legend=EC10.legend, lty=1, col="red")
+  }
+  if(add.EC10==TRUE & add.NEC==TRUE){
+    abline(v=NEC, col = "red", lty=c(3,1,3))  
+    abline(v=EC10, col = "orange", lty=c(3,1,3)) 
+    
+    legend(legend.loc, bty="n",
+           legend=c(NEC.legend, EC10.legend), 
+           lty=1, col=c("red", "orange"))
+  }
 }
