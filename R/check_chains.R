54366e00af16fe43d48dd8c16c7fd91f72054836
beckyfisher <rebeccafisher76@gmail.com>
2020-02-20 14:06:52 +0800

    Added capacity to deal with jagsMANECfit oject classes

--- R/check_chains.R
+++ R/check_chains.R
@@ -24,32 +24,67 @@
 #' @return A plot of MCMC chains and ACF diagrams for each element in params.
 
 check.chains <- function(X, name=""){
-  params <- X$params
-  x <- X$sims.array
-  num.chains <- ncol(x[,,params[1]])
+  
+  if(class(X)=="jagsNECfit"){
+      params <- X$params
+      x <- X$sims.array
+      num.chains <- ncol(x[,,params[1]])
 
-  par(mfrow=c(length(params),2),mar=c(0,5,0.5,0.5),oma=c(4,0,2,0))
-  for (i in 1:length(params)){
-   	x1          <- as.vector(x[,,params[i]])
-  	chain.id     <- rep(1:num.chains, each = nrow(x[,,params[i]]))
-    num.lags <- length(acf(x[,,params[i]][,1], plot = FALSE)$lag)
+      par(mfrow=c(length(params),2),mar=c(0,5,0.5,0.5),oma=c(4,0,2,0))
+      for (i in 1:length(params)){
+   	    x1          <- as.vector(x[,,params[i]])
+  	    chain.id     <- rep(1:num.chains, each = nrow(x[,,params[i]]))
+        num.lags <- length(acf(x[,,params[i]][,1], plot = FALSE)$lag)
 
-    # plot the chains
-    plot(1:nrow(x[,,i]),rep(NA,nrow(x[,,params[i]])), xaxt="n",
+        # plot the chains
+        plot(1:nrow(x[,,i]),rep(NA,nrow(x[,,params[i]])), xaxt="n",
          ylim=range(x1), main="", xlab="", ylab=params[i])
-    if(i == length(params)){axis(side=1)}
-    for(k in 1:num.chains){
-        lines(1:nrow(x[,,params[i]]), x1[which(chain.id==k)],col=k)}
+        if(i == length(params)){axis(side=1)}
+        for(k in 1:num.chains){
+            lines(1:nrow(x[,,params[i]]), x1[which(chain.id==k)],col=k)}
 
-    # plot the acf
-    plot(1:num.lags,rep(NA,num.lags), xaxt="n",
-        ylim=c(0,1),xlab="lag",ylab="correlation",main="")
-    if(i == length(params)){axis(side=1)}
-     for (j in 1:num.chains){
-      acf.j <- acf(x[,,params[i]][,j], plot = FALSE)
-      lines(acf.j$lag, acf.j$acf,col=j )
+        # plot the acf
+        plot(1:num.lags,rep(NA,num.lags), xaxt="n",
+           ylim=c(0,1),xlab="lag",ylab="correlation",main="")
+       if(i == length(params)){axis(side=1)}
+        for (j in 1:num.chains){
+         acf.j <- acf(x[,,params[i]][,j], plot = FALSE)
+          lines(acf.j$lag, acf.j$acf,col=j )
+         }
+       }
+     mtext(name,side=3,outer=T)   
+  }
+  if(class(X)=="jagsMANECfit"){
+   for(m in 1:length(X$mod.fits)){
+     X.m <- X$mod.fits[[m]]
+     params <- X.m$params
+     x <- X.m$sims.array
+     num.chains <- ncol(x[,,params[1]])
+     x11()
+     par(mfrow=c(length(params),2),mar=c(0,5,0.5,0.5),oma=c(4,0,2,0))
+     for (i in 1:length(params)){
+       x1          <- as.vector(x[,,params[i]])
+       chain.id     <- rep(1:num.chains, each = nrow(x[,,params[i]]))
+       num.lags <- length(acf(x[,,params[i]][,1], plot = FALSE)$lag)
+       
+       # plot the chains
+       plot(1:nrow(x[,,i]),rep(NA,nrow(x[,,params[i]])), xaxt="n",
+            ylim=range(x1), main="", xlab="", ylab=params[i])
+       if(i == length(params)){axis(side=1)}
+       for(k in 1:num.chains){
+         lines(1:nrow(x[,,params[i]]), x1[which(chain.id==k)],col=k)}
+       
+       # plot the acf
+       plot(1:num.lags,rep(NA,num.lags), xaxt="n",
+            ylim=c(0,1),xlab="lag",ylab="correlation",main="")
+       if(i == length(params)){axis(side=1)}
+       for (j in 1:num.chains){
+         acf.j <- acf(x[,,params[i]][,j], plot = FALSE)
+         lines(acf.j$lag, acf.j$acf,col=j )
+       }
      }
-    }
- mtext(name,side=3,outer=T)
+     mtext(names(X$mod.fits)[m],side=3,outer=T)        
+   } 
+  }
 }
 
