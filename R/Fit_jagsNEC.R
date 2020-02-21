8cfc9f912f42cd3671a3f60c8d99b333d1c2cb6b
beckyfisher <rebeccafisher76@gmail.com>
2020-02-21 16:16:05 +0800

    turned off initial value failure warning

--- R/Fit_jagsNEC.R
+++ R/Fit_jagsNEC.R
@@ -267,9 +267,9 @@ fit.jagsNEC <- function(data,
     all.Js <- c(all.Js, list(J1))   
     if(max(unlist(check.mixing(J1)$cv.test))==1){class(J1)="try-error"}
   }
-  if(class(J1)=="try-error"){ 
-   warning("The generated init.fun failed to yield a valid model. Model based on default jags initial values")
-  }
+  #if(class(J1)=="try-error"){ 
+  # warning("The generated init.fun failed to yield a valid model. Model based on default jags initial values")
+  #}
   # if the attempt fails try 10 more times
   #warn = getOption("warn")
   #options(warn=-1)
